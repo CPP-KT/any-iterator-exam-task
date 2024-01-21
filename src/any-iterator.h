48fe8715a110de0cd1f17f2c22f3ae97e22e5ca2
Artem Koton <kotonartem@protonmail.com>
2024-01-21 03:19:21 +0300

    Update header

--- src/any-iterator.h
+++ src/any-iterator.h
@@ -1,14 +1,18 @@
+#pragma once
 
 template <typename T, typename Tag>
-struct any_iterator {
-  // replace void with needed type
+class any_iterator {
+public:
   using value_type = void;
-  using pointer = void*;
+  using pointer = void;
   using reference = void;
   using difference_type = void;
   using iterator_category = void;
 
+public:
   any_iterator();
+
+  ~any_iterator();
 
   template <typename It>
   any_iterator(It);
@@ -16,13 +20,13 @@ struct any_iterator {
   any_iterator(const any_iterator& other);
   any_iterator(any_iterator&& other);
 
+  template <typename It>
+  any_iterator& operator=(It it);
+
   any_iterator& operator=(any_iterator&& other);
   any_iterator& operator=(const any_iterator& other);
 
   void swap(any_iterator& other) noexcept;
-
-  template <typename It>
-  any_iterator& operator=(It it);
 
   const T& operator*() const;
   T& operator*();
@@ -39,7 +43,7 @@ struct any_iterator {
   template <typename TT, typename TTag>
   friend bool operator!=(const any_iterator<TT, TTag>& a, const any_iterator<TT, TTag>& b);
 
-  // note: next operators must compile ONLY for appropriate iterator tags
+  // note: following operators must compile ONLY for appropriate iterator tags
 
   template <typename TT, typename TTag>
   friend any_iterator<TT, TTag>& operator--(any_iterator<TT, TTag>&);
@@ -65,6 +69,4 @@ struct any_iterator {
   template <typename TT, typename TTag>
   friend typename any_iterator<TT, TTag>::difference_type operator-(const any_iterator<TT, TTag>& a,
                                                                     const any_iterator<TT, TTag>& b);
-
-  ~any_iterator();
 };
